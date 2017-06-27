53db337e5bc4338614c839726fdad945f5791c3c
bienflorencia <greendoors@gmail.com>
2017-06-27 10:09:40 -0300

    Add files via upload

---
+++ run_ecamber.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+#comandos para correr ecamber.py a partir del [directorio] de genomas 
+
+python ecamber.py -a f -d [directorio] -w 4
+python ecamber.py -a ph1 -d [directorio] -w 4 -as patric
+python ecamber.py -a ph2 -w 4 -d [directorio] -as patric
+python ecamber.py -a out -d [directorio] -w 4 -as patric -step 3

c3ea91f1a948a141ecc58d0df717f28a8bdc59dc
bienflorencia <greendoors@gmail.com>
2017-08-23 14:49:19 -0300

    Update run_ecamber.py

--- run_ecamber.py
+++ run_ecamber.py
@@ -4,8 +4,7 @@ import sys
 import os
 
 #comandos para correr ecamber.py a partir del [directorio] de genomas 
-
-python ecamber.py -a f -d [directorio] -w 4
-python ecamber.py -a ph1 -d [directorio] -w 4 -as patric
-python ecamber.py -a ph2 -w 4 -d [directorio] -as patric
-python ecamber.py -a out -d [directorio] -w 4 -as patric -step 3
+os.system('python ecamber.py -a f -d [directorio] -w 4')
+os.system('python ecamber.py -a ph1 -d [directorio] -w 4 -as patric')
+os.system('python ecamber.py -a ph2 -w 4 -d [directorio] -as patric')
+os.system('python ecamber.py -a out -d [directorio] -w 4 -as patric -step 3')
